565f78f17b44ecace08b2ff31bafd14b27a3b55a
Travis Truman <trumant@gmail.com>
2012-03-18 19:58:48 -0400

    deleting crap

--- roles/lucid64.rb
+++
@@ -1,9 +0,0 @@
-name "lucid64"
-description "lucid64 base box"
-run_list(
-  "role[base]"
-)
-# Attributes applied if the node doesn't have it set already.
-#default_attributes()
-# Attributes applied no matter what the node has set already.
-#override_attributes()
